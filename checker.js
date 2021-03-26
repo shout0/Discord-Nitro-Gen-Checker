aaf8fe3ad77a4ad84ddfb3a8f8c3ba6746637c14
Catatomik <felix.lasseux@gmail.com>
2021-03-26 18:50:27 +0100

    :ambulance: Proxies crashing error

--- checker.js
+++ checker.js
@@ -195,7 +195,7 @@ if (proxy) {
     mkdirp.sync(__dirname+proxiesfile.match(/.*\//g)[0])
     fs.closeSync(fs.openSync(__dirname+proxiesfile, 'w'))
 }
-const proxies = proxy ? fs.readFileSync(__dirname+proxiesfile, { encoding: 'utf-8' }).split('\n').filter(p => p).map((proxy, i) => new Proxy(proxy, i)) : []
+let proxies = proxy ? fs.readFileSync(__dirname+proxiesfile, { encoding: 'utf-8' }).split('\n').filter(p => p).map((proxy, i) => new Proxy(proxy, i)) : []
 
 async function grabProxies() {
 
