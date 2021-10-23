2e894890864749017461b80bc62ec0882df37caa
Catatomik <cata@catadev.ga>
2021-10-23 19:57:55 +0200

    ⬆️ Bump node-fetch from 2.6.1 to 3.0.0

--- checker.js
+++ checker.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const mkdirp = require('mkdirp')
 const ProxyAgent = require('proxy-agent');
 const fs = require('fs')
